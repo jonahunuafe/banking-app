0afb6fe7a3705468ce6f9063eb5b7c6d97c705e9
Jonah Unuafe <fejisfejiro@gmail.com>
2024-10-02 00:49:32 +0100

    create MobileNav component structure

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,6 @@
+import MobileNav from "@/components/MobileNav";
 import SideBar from "@/components/SideBar";
+import Image from "next/image";
 
 export default function RootLayout({
   children,
@@ -12,7 +14,23 @@ export default function RootLayout({
       <SideBar 
         user={loggedIn}
       />
-      {children}
+
+      <div className="flex size-full flex-col">
+        <div className="root-layout">
+          <Image 
+            src="/icons/logo.svg"
+            width={30}
+            height={30}
+            alt="menu icon"
+          />
+          <div>
+            <MobileNav 
+              user={loggedIn}
+            />
+          </div>
+        </div>
+        {children}
+      </div>
     </main>
   );
 }

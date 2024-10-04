38183bb81445c0b381bf7ec779b16df197abcb80
Jonah Unuafe <fejisfejiro@gmail.com>
2024-10-04 20:16:56 +0100

    implement Authform using shadcn Form component

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -77,7 +77,27 @@ const AuthForm = ({ type }: {type: string}) => {
           </div>
         ) : (
           <>
-            FORM
+            <Form {...form}>
+              <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                <FormField
+                  control={form.control}
+                  name="username"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Username</FormLabel>
+                      <FormControl>
+                        <Input placeholder="shadcn" {...field} />
+                      </FormControl>
+                      <FormDescription>
+                        This is your public display name.
+                      </FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <Button type="submit">Submit</Button>
+              </form>
+            </Form>
           </>
         )
       }

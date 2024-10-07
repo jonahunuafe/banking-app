0b1e439240c765cd9b7acdf306cdf64a1061fcfa
Jonah Unuafe <fejisfejiro@gmail.com>
2024-10-07 09:02:44 +0100

    create custom input component

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -9,21 +9,13 @@ import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 
 import { Button } from "@/components/ui/button"
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
+import { Form } from "@/components/ui/form";
+import CustomInput from './CustomInput';
+
  
 const formSchema = z.object({
-  username: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
-  }),
+  email: z.string().email(),
+  password: z.string(),
 })
 
 const AuthForm = ({ type }: {type: string}) => {
@@ -33,7 +25,8 @@ const AuthForm = ({ type }: {type: string}) => {
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      username: "",
+      email: "",
+      password: "",
     },
   })
  
@@ -79,22 +72,62 @@ const AuthForm = ({ type }: {type: string}) => {
           <>
             <Form {...form}>
               <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-                <FormField
+                {/* <FormField
                   control={form.control}
-                  name="username"
+                  name="email"
                   render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Username</FormLabel>
-                      <FormControl>
-                        <Input placeholder="shadcn" {...field} />
-                      </FormControl>
-                      <FormDescription>
-                        This is your public display name.
-                      </FormDescription>
-                      <FormMessage />
-                    </FormItem>
+                    <div className="form-item">
+                      <FormLabel className="form-label">
+                        Email
+                      </FormLabel>
+                      <div className="flex w-full flex-col">
+                        <FormControl>
+                          <Input 
+                            placeholder="Enter your email"
+                            className="input-class"
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage 
+                          className="form-message mt-2"
+                        />
+                      </div>
+                    </div>
                   )}
+                /> */}
+
+                <CustomInput
+                  form, 
+                  name, 
+                  label, 
+                  placeholder 
                 />
+                <CustomInput />
+
+                {/* <FormField
+                  control={form.control}
+                  name="password"
+                  render={({ field }) => (
+                    <div className="form-item">
+                      <FormLabel className="form-label">
+                        Password
+                      </FormLabel>
+                      <div className="flex w-full flex-col">
+                        <FormControl>
+                          <Input 
+                            placeholder="Enter your password"
+                            className="input-class"
+                            type="password"
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage 
+                          className="form-message mt-2"
+                        />
+                      </div>
+                    </div>
+                  )}
+                /> */}
                 <Button type="submit">Submit</Button>
               </form>
             </Form>
---
+++ components/CustomInput.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+
+const CustomInput = ({ form, name, label, placeholder }) => {
+  return (
+    <FormField
+      control={form.control}
+      name={name}
+      render={({ field }) => (
+        <div className="form-item">
+          <FormLabel className="form-label">
+            {label}
+          </FormLabel>
+          <div className="flex w-full flex-col">
+            <FormControl>
+              <Input 
+                placeholder={placeholder}
+                className="input-class"
+                {...field}
+              />
+            </FormControl>
+            <FormMessage 
+              className="form-message mt-2"
+            />
+          </div>
+        </div>
+      )}
+    />
+  )
+}
+
+export default CustomInput;

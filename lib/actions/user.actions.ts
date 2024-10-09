5b8d90e2f1a67b6ab3a90ab63b07ae83551b63b1
Jonah Unuafe <fejisfejiro@gmail.com>
2024-10-09 17:09:35 +0100

    implement user sign up

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -5,9 +5,14 @@ import { createAdminClient, createSessionClient } from "../appwrite";
 import { cookies } from "next/headers";
 import { parseStringify } from "../utils";
 
-export const signIn = async () => {
+export const signIn = async ({ email, password }: signInProps) => {
   try {
     // Mutation / Database / Make fetch
+    const { account } = await createSessionClient();
+
+    const response = await account.createEmailPasswordSession(email, password);
+
+    return parseStringify(response)
   } catch(error) {
     console.error("Error", error)
   }

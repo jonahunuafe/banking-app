ca763a41c939de39be5ea08cbd1acd5d7386f8d3
Jonah Unuafe <fejisfejiro@gmail.com>
2024-10-10 09:37:15 +0100

    changed RootLayout to an async function to fetch loggedInUser

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -8,6 +8,10 @@ const Footer = ({ user, type = "desktop" }: FooterProps) => {
           {user.firstName[0]}
         </p>
       </div>
+
+      <div className={type === "mobile" ? "footer_email-mobile" : "footer_email"}>
+        <h1 className="text-14 truncate font-normal text-gray-600">{user.name}</h1>
+      </div>
     </footer>
   )
 }

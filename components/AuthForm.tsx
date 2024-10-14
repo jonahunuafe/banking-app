9d1b5b078951234926e28cac3f2f04386aa9bdd7
Jonah Unuafe <fejisfejiro@gmail.com>
2024-10-14 20:18:20 +0100

    form a new user object on signed up

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -41,8 +41,21 @@ const AuthForm = ({ type }: {type: string}) => {
     setIsLoading(true)
     try {
       // Sign up with appwrite & create a plain link token
+      
       if(type === "sign-up") {
-        const newUser = await signUp(data);
+        const userData = {
+          firstName: data.firstName!,
+          lastName: data.lastName!,
+          address1: data.address1!,
+          city: data.city!,
+          state: data.state!,
+          postalCode: data.postalCode!,
+          dateOfBirth: data.dateOfBirth!,
+          ssn: data.ssn!,
+          email: data.email,
+          password: data.password,
+        }
+        const newUser = await signUp(userData);
 
         setUser(newUser);
       }
